81bfbee66adebdbafd65d6842526d7d60028ea8c
Cool Lin <andrzejkom2016@gmail.com>
2020-04-21 11:01:41 +0200

    add new column-> price

--- Shop/product_insert.sql
+++ Shop/product_insert.sql
@@ -1,4 +1,8 @@
-INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status) 
-    VALUES(uuid_generate_v4(),'Small Ripe bananas','Fresh Food','Fruits','Bananas',100,'item','A');
-INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status) 
-    VALUES(uuid_generate_v4(),'Big Ripe bananas','Fresh Food','Fruits','Bananas',100,'item','A');
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'Emperor Orange','Fresh Food','Fruits','Citrus Fruit',100,'item','A',2.50);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'Lemons','Fresh Food','Fruits','Citrus Fruit',100,'kg','A',8.50);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'Red Oranges','Fresh Food','Fruits','Citrus Fruit',100,'item','A',9.50);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'Clementine','Fresh Food','Fruits','Citrus Fruit',100,'kg','A',11.50);
