418242ee74723ba7dc65361f71e84b7427614593
AgnieszkaAnd <ag2rio@gmail.com>
2020-04-21 21:34:10 +0200

    added insert commands for Bakery division in the Shop

---
+++ Shop/Resources/FMCGdataBakery.sql
@@ -0,0 +1,40 @@
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'WhiteFarmhouseBread','Bakery','Bread','WhiteBread',1,'item','A',3.5);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'Slicedbread','Bakery','Bread','WhiteBread',1,'item','A',4.5);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'FitBread','Bakery','Bread','WholegrainBread',1,'item','A',6.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'Slicedbread','Bakery','Bread','WholegrainBread',1,'item','A',4.5);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'Ciabatta','Bakery','Bread','WhiteBread',1,'item','A',2.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'AllButtercroissant','Bakery','Pastries','Croissants',1,'item','A',1.5);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'AlmondCroissant','Bakery','Pastries','Croissants',1,'item','A',2.5);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'CinammonBun','Bakery','Pastries','Brioche',1,'item','A',3.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'CardammonBun','Bakery','Pastries','Brioche',1,'item','A',3.5);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'PortugeseCustard','Bakery','Pastries','Brioche',1,'item','A',2.5);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'StrawberryIcedDoughnut','Bakery','Cookies','Doughnuts',1,'package','A',2.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'BerryIcedDoughnut','Bakery','Cookies','Doughnuts',1,'package','A',2.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'CaramelIcedDoughnut','Bakery','Cookies','Doughnuts',1,'package','A',2.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'TripleChocolateCookie','Bakery','Cookies','AmericanCookies',1,'package','A',19.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'WhiteChocolateCookie','Bakery','Cookies','AmericanCookies',1,'package','A',19.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'CheeseCake','Bakery','Cakes','BirthdayCakes',1,'item','A',15.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'DoubleChocolateCake','Bakery','Cakes','BirthdayCakes',1,'item','A',20.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'ApplePie','Bakery','Cakes','PiesandTarts',1,'item','A',9.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'RhubarbCramble','Bakery','Cakes','PiesandTarts',1,'item','A',9.0);
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status, price) 
+    VALUES(uuid_generate_v4(),'RaspberryPie','Bakery','Cakes','PiesandTarts',1,'item','A',10.0);
