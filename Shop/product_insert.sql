9f1419e20136a27e4979057ee66c5a68ac011733
Cool Lin <andrzejkom2016@gmail.com>
2020-04-21 09:05:34 +0200

    utworzenie pliku .sql do populacji tabeli bazy danych

---
+++ Shop/product_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status) 
+    VALUES(uuid_generate_v4(),'Small Ripe bananas','Fresh Food','Fruits','Bananas',100,'item','A');
+INSERT INTO products(product_uid,name,division,brigade,battalion,quantity,unit,status) 
+    VALUES(uuid_generate_v4(),'Big Ripe bananas','Fresh Food','Fruits','Bananas',100,'item','A');
