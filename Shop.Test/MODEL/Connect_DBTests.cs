533eedb61ea2b7daf2e9661c6f2977d148a20a3c
Cool Lin <andrzejkom2016@gmail.com>
2020-04-24 14:17:13 +0200

    up2

--- Shop.Test/MODEL/Connect_DBTests.cs
+++ Shop.Test/MODEL/Connect_DBTests.cs
@@ -16,11 +16,9 @@ namespace Shop.Test
         public void ReadTable_NotExistingColumnName_Catches_Exception()
         {
             //TODO - Agnieszka
+        
         }
-        [Fact]
-        public void test()
-        {
-            
-        }
+        
+        
     }
 }
