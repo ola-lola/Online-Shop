ef45b61b22bef0d714182caeb8de3ff60f40294e
Ola <olagrzegorczyk93@gmail.com>
2020-04-24 15:17:01 +0200

    Small change in sql file

--- Shop/Resources/AlcoholsToInsert.sql
+++ Shop/Resources/AlcoholsToInsert.sql
@@ -1,56 +1,56 @@
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Scandi Moscato Sweet','Alcohol','Wine','Sweet Wine',1,'bottle','A',11.0);
+    VALUES (uuid_generate_v4(),'Scandi Moscato Sweet','Alcohol','Wine','Sweet Wine',1,'bot','A',11.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Korbel Sweet Rose','Alcohol','Wine','Sweet Wine',1,'bottle','A',10.9);
+    VALUES (uuid_generate_v4(),'Korbel Sweet Rose','Alcohol','Wine','Sweet Wine',1,'bot','A',10.9);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Martini Rossi Sweet','Alcohol','Wine','Sweet Wine',1,'bottle','A',11.2);
+    VALUES (uuid_generate_v4(),'Martini Rossi Sweet','Alcohol','Wine','Sweet Wine',1,'bot','A',11.2);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Mirapiana Maremma Toscana','Alcohol','Wine','Red Dry Wine',1,'bottle','A',14.0);
+    VALUES (uuid_generate_v4(),'Mirapiana Maremma Toscana','Alcohol','Wine','Red Dry Wine',1,'bot','A',14.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','La Cupola Pinot Nero','Alcohol','Wine','Red Dry Wine',1,'bottle','A',15.0);
+    VALUES (uuid_generate_v4(),'La Cupola Pinot Nero','Alcohol','Wine','Red Dry Wine',1,'bot','A',15.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Orpheus Cabernet Sauvignon','Alcohol','Wine','Red Dry Wine',1,'bottle','A',14.9);
+    VALUES (uuid_generate_v4(),'Orpheus Cabernet Sauvignon','Alcohol','Wine','Red Dry Wine',1,'bot','A',14.9);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Orpheus Chardonnay','Alcohol','Wine','White Dry Wine',1,'bottle','A',15.0);
+    VALUES (uuid_generate_v4(),'Orpheus Chardonnay','Alcohol','Wine','White Dry Wine',1,'bot','A',15.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Nos Racines Sauvignon Blanc','Alcohol','Wine','White Dry Wine',1,'bottle','A',16.0);
+    VALUES (uuid_generate_v4(),'Nos Racines Sauvignon Blanc','Alcohol','Wine','White Dry Wine',1,'bot','A',16.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Woodhaven Chardonnay','Alcohol','Wine','White Dry Wine',1,'bottle','A',16.0);
+    VALUES (uuid_generate_v4(),'Woodhaven Chardonnay','Alcohol','Wine','White Dry Wine',1,'bot','A',16.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Las Chanas Tinto','Alcohol','Wine','Semi Sweet Wine',1,'bottle','A',15.0);
+    VALUES (uuid_generate_v4(),'Las Chanas Tinto','Alcohol','Wine','Semi Sweet Wine',1,'bot','A',15.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Kendermanns Riesling Kabinett','Alcohol','Wine','Semi Sweet Wine',1,'bottle','A',16.5);
+    VALUES (uuid_generate_v4(),'Kendermanns Riesling Kabinett','Alcohol','Wine','Semi Sweet Wine',1,'bot','A',16.5);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','El Circo Tempranillo','Alcohol','Wine','Semi Sweet Wine',1,'bottle','A',16.0);
+    VALUES (uuid_generate_v4(),'El Circo Tempranillo','Alcohol','Wine','Semi Sweet Wine',1,'bot','A',16.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Moet & Chandon Ice Imperial','Alcohol','Wine','Semi Sweet Wine',1,'bottle','A',37.0);
+    VALUES (uuid_generate_v4(),'Moet & Chandon Ice Imperial','Alcohol','Wine','Semi Sweet Wine',1,'bot','A',37.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Kendermanns Liebfraumilch','Alcohol','Wine','Semi Sweet Wine',1,'bottle','A',14.0);
+    VALUES (uuid_generate_v4(),'Kendermanns Liebfraumilch','Alcohol','Wine','Semi Sweet Wine',1,'bot','A',14.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Zubrowka Bison Grass','Alcohol','Vodka','Polish Vodka',1,'bottle','A',10.0);
+    VALUES (uuid_generate_v4(),'Zubrowka Bison Grass','Alcohol','Vodka','Polish Vodka',1,'bot','A',10.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Zubrowka Gold','Alcohol','Vodka','Polish Vodka',1,'bottle','A',10.0);
+    VALUES (uuid_generate_v4(),'Zubrowka Gold','Alcohol','Vodka','Polish Vodka',1,'bot','A',10.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Zubrowka White','Alcohol','Vodka','Polish Vodka',1,'bottle','A',10.0);
+    VALUES (uuid_generate_v4(),'Zubrowka White','Alcohol','Vodka','Polish Vodka',1,'bot','A',10.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Soplica Szlachecka','Alcohol','Vodka','Polish Vodka',1,'bottle','A',10.0);
+    VALUES (uuid_generate_v4(),'Soplica Szlachecka','Alcohol','Vodka','Polish Vodka',1,'bot','A',10.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Absolwent','Alcohol','Vodka','Polish Vodka',1,'bottle','A',10.5);
+    VALUES (uuid_generate_v4(),'Absolwent','Alcohol','Vodka','Polish Vodka',1,'bot','A',10.5);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Zytniowka','Alcohol','Vodka','Polish Vodka',1,'bottle','A',9.0);
+    VALUES (uuid_generate_v4(),'Zytniowka','Alcohol','Vodka','Polish Vodka',1,'bot','A',9.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Wyborowa','Alcohol','Vodka','Polish Vodka',1,'bottle','A',11.0);
+    VALUES (uuid_generate_v4(),'Wyborowa','Alcohol','Vodka','Polish Vodka',1,'bot','A',11.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Pan Tadeusz','Alcohol','Vodka','Polish Vodka',1,'bottle','A',12.0);
+    VALUES (uuid_generate_v4(),'Pan Tadeusz','Alcohol','Vodka','Polish Vodka',1,'bot','A',12.0);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Lubelska Lemon','Alcohol','Vodka','Flavoured Vodka',1,'bottle','A',11.5);
+    VALUES (uuid_generate_v4(),'Lubelska Lemon','Alcohol','Vodka','Flavoured Vodka',1,'bot','A',11.5);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Absolut Miami','Alcohol','Vodka','Flavoured Vodka',1,'bottle','A',72.5);
+    VALUES (uuid_generate_v4(),'Absolut Miami','Alcohol','Vodka','Flavoured Vodka',1,'bot','A',72.5);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Zubrowka Apple','Alcohol','Vodka','Flavoured Vodka',1,'bottle','A',11.5);
+    VALUES (uuid_generate_v4(),'Zubrowka Apple','Alcohol','Vodka','Flavoured Vodka',1,'bot','A',11.5);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Stumbras Rasberry','Alcohol','Vodka','Flavoured Vodka',1,'bottle','A',13.5);
+    VALUES (uuid_generate_v4(),'Stumbras Rasberry','Alcohol','Vodka','Flavoured Vodka',1,'bot','A',13.5);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Effen Blood Orange','Alcohol','Vodka','Flavoured Vodka',1,'bottle','A',27.5);
+    VALUES (uuid_generate_v4(),'Effen Blood Orange','Alcohol','Vodka','Flavoured Vodka',1,'bot','A',27.5);
 INSERT INTO products (product_uid,name,division,brigade,battalion,quantity,unit,status,price) 
-    VALUES ('uuid_generate_v4()','Ketel One Grapefruit','Alcohol','Vodka','Flavoured Vodka',1,'bottle','A',25.5);
+    VALUES (uuid_generate_v4(),'Ketel One Grapefruit','Alcohol','Vodka','Flavoured Vodka',1,'bot','A',25.5);
