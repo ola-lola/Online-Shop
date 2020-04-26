c4335710807023f34eb0a57a9b9c5793c5ab1737
AgnieszkaAnd <ag2rio@gmail.com>
2020-04-26 18:35:41 +0200

    fixes test project - no errors with using Xunit

--- Shop.Test/UnitTest1.cs
+++ Shop.Test/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using Shop;
 
 namespace Shop.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ReadTable_NotExistingTableName_Catches_Exception()
         {
-            
+            //TODO - Agnieszka
+        }
+        [Fact]
+        public void ReadTable_NotExistingColumnName_Catches_Exception()
+        {
+            //TODO - Agnieszka
         }
     }
 }
